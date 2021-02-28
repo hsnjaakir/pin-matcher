1ca21dc7c6ee82d6e5ec2fd834fdfd39268df83c
hsnjaakir <jakir0613@gmail.com>
2021-03-01 00:44:03 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const notifySection = document.getElementById("notify-section");
+notifySection.style.display = "none";
+function Random () {
+    const number = Math.random()*10000;
+    let value = Math.round(number);
+    let length = Math.log10(value)+1;
+    while (length < 4) {
+        value = value*10;
+        length = Math.log10(value)+1;
+}
+    return value;
+}
+const randomValue = Random ();
+
+const generatePin = document.getElementById("generatePin");
+generatePin.addEventListener('click', function (){
+    document.getElementById("randomText").value = randomValue;
+})
+
+function button (id) {
+    const x = document.getElementById(id);
+    x.addEventListener('click',function (){
+    const initialInput = document.getElementById("getPin").value;
+    const getPin = document.getElementById(id).innerText; 
+    document.getElementById("getPin").value = initialInput + getPin;
+})
+}
+
+button("zero");
+button("one");
+button("two");
+button("three");
+button("four");
+button("five");
+button("six");
+button("seven");
+button("eight");
+button("nine");
+
+const ifMatch = document.getElementById('ifMatch');
+ifMatch.addEventListener('click', function(){
+    if (randomValue == document.getElementById("getPin").value) {
+        notifySection.style.display = "block";
+    }
+})
